4d68e61c49eab19e9d952f61651f337b6624cc29
starryKey <Li_JinLin@Li-JinLindeMacBook-Pro.local>
2017-06-16 01:12:12 +0800

    学习属性相关

--- 05面向对象（上）/main.swift
+++ 05面向对象（上）/main.swift
@@ -101,7 +101,172 @@ stu1.name = "Jack"
 stu1.showStudentInfo()
 stu2.showStudentInfo()
 
+/*
+ tip  恒等运算符
+ swift 提供两种恒等运算符=== （等价于）和 !==（不等价于）用来检测两个常量或者边来干是否引用同一个类实例
+ */
+class Person3: NSObject {
+    
+}
+var per31 = Person3()
+var per32 = Person3()
+let per33 = per31
+let per34 = per32
+if (per33 == per31){
+    print("引用同一个实例")
+}
+if(per33 !== per34){
+    print("引用不同的实例")
+}
+//5.3.2 类和结构体对比
+/*
+ tips ：（1）、swift中类和结构体的共同点：
+            1、定义属性用于存储值
+            2、定义方法用于提供功能
+            3、定义下标用于通过下标语法访问值
+            4、定义构造函数用于生成初始化值
+            5、通过拓展以增加默认实现的功能
+            6、符合协议以对某类提供标准功能
+        （2）、与结构体相比，类还有一些结构体不具备的特征
+            1、继承允许一个类继承另一个类的特征
+            2、类型转换在运行时检查和解释一个类实例的类型
+            3、取消构造函数允许允许一个类实例四方任何其所分配的资源
+            4、引用计数允许对一个类的多次引用
+        （3）、结构体总是通过呗肤质的方式在代码中传递，因此请不要使用引用计数
+ */
 
+//5.3  属性
+//属性用于描述特定类、结构或者枚举的值。在swift中属性可以分为存储属性、计算属性、和类属性三种。
+//5.3.1存储属性（存储变量或者常量）
+struct Teacher{
+    let name = "Tom"
+    var age = 28
+    var home = "hanghzhou"
+    
+}
+var tea1 = Teacher()
+//tea1.name = "hh" error 注意name是常量
+tea1.age = 29
+tea1.home = "beijing"
+
+var tea2 = Teacher() //此时实例是let常量实例，属性值不能被修改，与类不一样      原因 ： 值类型
+//tea2.name = "zzz"error //注意name是常量
+//tea2.age = 30
+//tea2.home = "guangzhou"
+print(tea2)
+
+class Teacher2: NSObject {
+    let name = "Liming"
+    var age:Int = 30
+}
+let teac1 = Teacher2() //let 常量类型不能修改值,但是内部变量属性age可以修改，  原因 ： 引用类型
+//tea2.name = "Xiaohua"
+tea2.age = 40
+let teac2 = Teacher2()
+tea2.age = 40
+
+//5.3.2懒存储属性
+//swift提供了懒存储属性，懒存储属性是指当被第一次调用的时候才会计算器初始值的属性，一个懒存储属性通过在属性声明前面加上lazy来表识
+//当属性初始值因为外部的原因，在实例初始化完成之前不能确定，或则会房属性初始值需要复杂、大量的计算时，就可以使用懒出场属性
+//实例
+class PersonInfo
+{
+    var personinfoFileName = "info.text"
+}
+class PersonDataManager {
+    lazy var personInfo = PersonInfo()
+    var data = String()
+}
+let manager = PersonDataManager()
+manager.data += "some data"
+manager.data += "some more data"
+print(manager.data)
+print(manager.personInfo.personinfoFileName)
+//Objective-C的懒加载，其实就是调用成员属性的get方法，初始化值，而Swift的懒加载，是和Objective-C不同的
+//在 Swift 中我们使用在变量属性前加 lazy 关键字的方式来简单地指定延时加载.我们在第一次访问某个属性时，判断这个属性背后的存储是否已经存在，如果存在则直接返回，如果不存在则说明是首次访问，那么就进行初始化并存储后再返回。这样我们可以把这个属性的初始化时刻推迟，与包含它的对象的初始化时刻分开，以达到提升性能的目的。
+/*
+    tip:必须将懒存储属性声明为变量（var），因为属性的值在实例构造完成之前可能无法得到它，而常量属性在构造过程完成之前必须要有初始值，因此无法声明称懒存储属性
+ */
+//5.3.3计算属性
+//计算属性不存储值，而是提供了一个getter和setter来分别进行获取和设置其他属性的值。getter使用get关键字进行定义，setter使用set关键字进行定义
+//例（如果计算属性的setter方法没有将被设置的值定义一个名称，会默认使用newValue名称代替）
+class PersonA {
+    var length:Int = 10
+    var age:Int
+    {
+        get
+        {
+            return length * 2
+        }
+        set
+        {
+            length = newValue / 2
+        }
+    }
+    //只读计算属性
+    var height:Int
+    {
+        get
+        {
+            return length * 4
+        }
+    }
+    //只读属性可以直接忽略get
+    var height2:Int
+    {
+        return length * 4
+    }
+}
+var personA = PersonA()
+//personA.height = 10//只读属性不能赋值
+personA.age = 20
+print(personA.height)
+/*
+    tip: 计算属性必须使用var关键字定义，包括只读计算属性，因为它们的值时不固定的，let关键字只能用来声明存储的属性，表示初始化以后不呢个再被修改了
+ */
+//5.3.4 属性观察器
+//属性观察器可以用来监控和响应初始化的属性值的变化，当属性的值发生改变的时候，以调用事先写好的代码额外执行一些操作并对此进行响应
+//属性观察器包括didSet和willSet，其中属性值改变之前会触发willSet，之后会触发didSet。
+/*
+    tips：属性观察器的特点
+    1、给属性添加观察器必须要知道属性的类型，否则编译失败
+    2、willSet可以带一个newName的参数，没有的话，该参数默认命名为newValue
+    3、didSet可以带一个oldName的参数，没有的话，该参数默认命名为oldValue
+    4、属性初始化时，willSet 、didSet不会调用。只有在初始化上下文之外，当设置属性值时才会调用。另外，在didSet的实现体背给属性赋值，也不会再次调用属性的
+    5、即使设置的值和原来的值相同，didSet和willSet也会被调用
+ */
+//例
+class PersonB{
+
+    //普通属性
+    var name:String?
+    var age:Int = 18
+    {
+        //在age属性发生变化之前
+        willSet
+        {
+            print("将要设置的年龄的值为\(newValue)")
+        }
+        //在age属性发生变化之后，更新一下name属性
+        didSet
+        {
+            if age < 16 {
+                name = "Tom"
+                print("\(age)岁的\(name!)")
+            } else {
+                name = "Jack"
+                print(name ?? "John")
+            }
+            print("\(name!)的年龄从\(oldValue)改为了\(age)")
+        }
+    }
+}
+let perB = PersonB()
+perB.age = 10
+perB.age = 20
+/*
+    tip: 如果在didSet观察器里为属性赋值，这个值会替换观察器之前那设置的值
+ */
 
 
 
